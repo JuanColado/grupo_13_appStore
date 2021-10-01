e481ac62b2f6db4fb79d3c024a9c1d4d8af777dd
MartinaFumiere <martifumiere97@gmail.com>
2021-09-30 22:24:42 -0300

    sprint 3.1

--- app.js
+++ app.js
@@ -3,16 +3,23 @@ const app = express();
 const path = require('path');
 app.use(express.static("public"));
 
+app.set("view engine", "ejs");
+
 app.listen(3050,console.log("Esta corriendo en el puerto 3050"));
+
 app.get("/",function(req,res){
-    res.sendFile((path.join(__dirname,'/views','/index.html')))})
+        res.render('index')})
 app.get("/index",function(req,res){
-    res.sendFile((path.join(__dirname,'/views/index.html')))})
+    res.render('index')})
 app.get("/login",function(req,res){
-    res.sendFile((path.join(__dirname,'/views','/login.html')))})
+    res.render('login')})
+
 app.get("/productCart",function(req,res){
-    res.sendFile((path.join(__dirname,'/views','/productCart.html')))})
+    res.render('productCart')})
+
 app.get("/productDetail",function(req,res){
-    res.sendFile((path.join(__dirname,'/views','/productDetail.html')))})
+    res.render('productDetail')})
+
 app.get("/register",function(req,res){
-    res.sendFile((path.join(__dirname,'/views','/register.html')))})
+    res.render('register')})
+
--- retro.md
+++ retro.md
@@ -1,6 +1,13 @@
-Retro primer Sprint:
+Retro Primer Sprint:
 1. Comenzar a hacer: Fijar horarios de reuniones grupales 3 veces por semana.
 2. Hacer más: Organizarnos individual y grupalmente las tareas a realizar para no mantener un esquema prolijo de trabajo. 
 3. Continuar haciendo: Tomar la iniciativa para comenzar a realizar las tareas del sprint.
 4. Hacer menos: Que las reuniones grupales sean mas enfocadas en el trabajo del Sprint.
 5. Dejar de hacer: Procastinar.
+
+Retro Segundo Sprint:
+1. Comenzar a hacer: empezar con anterioridad a preparar los sprints, no dejarlo para la ultima semana.Implementar una revision de codigos cruzados.
+2. Hacer más: enfocarnos en la realizacion de los objetuvos puntuales. 
+3. Continuar haciendo: reuniones asi de seguido. Trabajar en equipo.
+4. Hacer menos: Hablar de temas irrelevantes.
+5. Dejar de hacer: No determinar el horario de las reuniones cuando decidimos el dia que nos reunimos.
