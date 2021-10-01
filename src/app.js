7ab32ecfc99be6e8dc576608390ab093be2b06b1
MartinaFumiere <martifumiere97@gmail.com>
2021-10-01 19:14:08 -0300

    Sprint 3.2

---
+++ src/app.js
@@ -0,0 +1,24 @@
+
+const express = require('express');
+const app = express();
+const path = require('path');
+const mainRouter =  require ('../src/views/routes/mainRouter');
+const usersRouter =  require ('../src/views/routes/usersRouter');
+const productsRouter =  require ('../src/views/routes/productsRouter');
+
+app.use(express.static("../public"));
+
+app.set("view engine", "ejs");
+
+app.listen(3050,console.log("Esta corriendo en el puerto 3050"));
+
+app.get("/", mainRouter);
+
+app.get("/index",mainRouter);
+app.get("/login", usersRouter);
+
+app.get("/productCart", productsRouter);
+
+app.get("/productDetail", productsRouter);
+
+app.get("/register",usersRouter);
