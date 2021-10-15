b22782f4c9a8550141aeb4f571cda160076e6b83
JuanColado <juanicolado@gmail.com>
2021-10-14 22:50:52 -0300

    avanzando 1231

---
+++ src/controllers/productsController.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const productsFilePath = path.resolve(__dirname, '../data/products.json');
+const fs = require("fs");
+const productsJson = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+const productsController = {
+    productCart: (req, res) => res.render('productCart'), 
+    productDetail: (req, res) => res.render('productDetail'), 
+    }
+    module.exports= productsController;
