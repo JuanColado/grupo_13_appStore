6fec95270876bb93fe918484d3f110e9fc2def65
MartinaFumiere <martifumiere97@gmail.com>
2021-10-25 17:55:43 -0300

    sprint4.4

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,11 +1,16 @@
 const path = require('path');
 const productsFilePath = path.resolve(__dirname, '../data/products.json');
 const fs = require("fs");
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+
 const productsController = {
     productCart: (req, res) => res.render('productCart'), 
     productDetail: (req, res) => res.render('productDetail'), 
-    // productsJson: JSON.parse(fs.readFileSync(productsFilePath, 'utf-8')),
+
     editProduct: (req,res) => res.render('editProduct'), 
     newProduct: (req,res) => res.render('newProduct'), 
+    products: (req,res) => res.render('products', {'products': products}),
+    
     }
     module.exports= productsController;
--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-const productsController = require ("../../controllers/productsController");
+const productsController = require ("../controllers/productsController");
+
 router.get('/productDetail', productsController.productDetail);
 router.get('/productCart', productsController.productCart);
-// router.get('/products', productsController.productsJson);
+
 router.get('/editProduct',productsController.editProduct);
 router.get('/newProduct',productsController.newProduct);
+
+router.get('/', productsController.products);
+
+
+
 module.exports = router;
